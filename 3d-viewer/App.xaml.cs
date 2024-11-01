22723773da2f89aa801fcc422fa2bb8f6da15d5c
UrekMazinoZh <zhukotynskyi.maksym.clg@chnu.edu.ua>
2024-11-01 09:09:15 +0200

    Add project files.

---
+++ 3d-viewer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _3d_viewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
