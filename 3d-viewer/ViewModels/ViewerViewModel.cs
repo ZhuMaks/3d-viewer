8965d90bb035134eb1cbab861eed9cb73ce3832a
flevrnght <maksyz15@gmail.com>
2024-11-15 08:53:50 +0200

    Added new load model method

--- 3d-viewer/ViewModels/ViewerViewModel.cs
+++ 3d-viewer/ViewModels/ViewerViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System.ComponentModel;
+﻿// ViewModels/ViewerViewModel.cs
+using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using System.Windows.Media.Media3D;
 using HelixToolkit.Wpf;
+using Microsoft.Win32;
 using ViewerApp.Models;
 using ViewerApp.ViewModels;
 
@@ -42,10 +45,29 @@ namespace _3d_viewer.ViewModels
 
         private void LoadModel()
         {
-            var modelImporter = new ModelImporter();
-            var model = modelImporter.Load("D:\\CollegeEdu\\viewer_wpf\\3d-viewer\\3d-viewer\\49-sting-sword-lowpoly.obj\\Sting-Sword-lowpoly.obj");  // Замість "path_to_model.obj" використайте шлях до вашої моделі
-            LoadedModel = model;
-            CurrentModel = new MeshModel(model);
+            // Використовуємо OpenFileDialog для вибору файлу моделі
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "OBJ Files (*.obj)|*.obj|FBX Files (*.fbx)|*.fbx|STL Files (*.stl)|*.stl|All Files (*.*)|*.*",
+                Title = "Select a 3D Model File"
+            };
+
+            // Перевіряємо, чи був файл вибраний користувачем
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    var modelImporter = new ModelImporter();
+                    var model = modelImporter.Load(openFileDialog.FileName);
+                    LoadedModel = model;
+                    CurrentModel = new MeshModel(model);
+                }
+                catch (Exception ex)
+                {
+                    // Обробка помилок, наприклад, показ повідомлення користувачу
+                    System.Windows.MessageBox.Show($"Error loading model: {ex.Message}", "Error", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+                }
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
